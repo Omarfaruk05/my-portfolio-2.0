ff3c71fd9ca3172c41caa81ab31dac326fed81a4
unknown <mdomarfaruk149518@gmail.com>
2024-07-13 21:51:14 +0600

    [updating]

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ const ProjectsPage = () => {
   const nums = [1, 2, 3, 4, 5, 6];
   return (
     <Container>
-      <div className="mt-8 space-y-12">
+      <div className="mt-8 mx-4 space-y-12">
         <h1 className="text-4xl font-semibold uppercase">Web App</h1>
         <div className="flex flex-wrap justify-center lg:justify-start gap-5">
           {nums.map((num, index) => (
@@ -13,7 +13,7 @@ const ProjectsPage = () => {
           ))}
         </div>
       </div>
-      <div className="my-8 space-y-12">
+      <div className="my-8 mx-4 space-y-12">
         <h1 className="text-4xl font-semibold uppercase">Mobile App</h1>
         <div className="flex flex-wrap justify-center lg:justify-start gap-5">
           {nums.map((num, index) => (
