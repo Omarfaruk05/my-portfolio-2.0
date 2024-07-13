ca3ba4352e41226f0c423e79338f31af8e5a963b
unknown <mdomarfaruk149518@gmail.com>
2024-07-13 19:48:09 +0600

    check pc

--- src/components/shared/NavigationButton.tsx
+++ src/components/shared/NavigationButton.tsx
@@ -5,7 +5,7 @@ import { IoArrowForwardCircleOutline } from "react-icons/io5";
 const NavigationButton = () => {
   return (
     <div>
-      <IoArrowForwardCircleOutline className="w-8 h-8 text-gray-400 hover:text-white" />
+      <IoArrowForwardCircleOutline className="w-8 h-8 text-gray-400 hover:text-blue-500" />
     </div>
   );
 };
